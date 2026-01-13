e5d475ea1f73b6ef5eb042aebdc2fb15f791fdf6
Kim Yongbeom <dernbu@gmail.com>
2026-01-13 16:01:31 +0000

    feat: remove @fontsource/inter

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,8 +1,6 @@
 import { createRouter } from "@tanstack/react-router";
 import { setupRouterSsrQueryIntegration } from "@tanstack/react-router-ssr-query";
 import * as TanstackQuery from "./integrations/tanstack-query/root-provider";
-import '@fontsource/inter';
-
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
